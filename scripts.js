465ad7cdb49f9dea39b5bf16d45055d3871f8999
Deepak Jaiswal. <deepujaiswal487@gmail.com>
2026-04-14 14:29:18 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,13 @@
 fetch("https://disease.sh/v3/covid-19/all")
-  .then(res => res.json())
+  .then(response => response.json())
   .then(data => {
-    document.body.innerHTML += `
+    document.getElementById("data").innerHTML = `
       <h2>Total Cases: ${data.cases}</h2>
       <h2>Recovered: ${data.recovered}</h2>
       <h2>Deaths: ${data.deaths}</h2>
     `;
   })
-  .catch(err => console.log(err));
+  .catch(error => {
+    document.getElementById("data").innerHTML = "Error loading data";
+    console.log(error);
+  });
